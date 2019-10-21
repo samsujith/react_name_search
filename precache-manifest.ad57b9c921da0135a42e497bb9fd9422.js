233345348ca81034a51d666116fa7f7d5b5f6781
samsujith <sujith.samuel92@gmail.com>
2019-10-21 22:38:18 +0530

    Updates

---
+++ precache-manifest.ad57b9c921da0135a42e497bb9fd9422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8270fb7e20818896650c9e9cd8695a4",
+    "url": "/react_name_search/index.html"
+  },
+  {
+    "revision": "09c48f9aa49688212208",
+    "url": "/react_name_search/static/css/main.f9727715.chunk.css"
+  },
+  {
+    "revision": "14f161e83eccdf221943",
+    "url": "/react_name_search/static/js/2.8ba5cd85.chunk.js"
+  },
+  {
+    "revision": "09c48f9aa49688212208",
+    "url": "/react_name_search/static/js/main.5295fd72.chunk.js"
+  },
+  {
+    "revision": "afa32be3f985beb5a7ac",
+    "url": "/react_name_search/static/js/runtime-main.664d2088.js"
+  }
+]);
