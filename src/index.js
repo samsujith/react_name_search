5bac02b60e8b00476925a5f74a55a00f04a76beb
samsujith <sujith.samuel92@gmail.com>
2019-10-25 09:41:58 +0530

    router configurations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {socialLogin} from './components/socialLogin.js';
+import Contact from './components/contact.js';
 import App from './App';
+import {Route,Link,BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path='/' component={socialLogin}></Route>
+            <Route path="/contact" component={Contact}></Route>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
